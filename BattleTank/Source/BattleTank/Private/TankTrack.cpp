172cc07c74e8667d33926d9ba7fafb788df64cc9
patakfal <patakfal@gmail.com>
2018-11-19 21:51:45 +0100

    188

--- BattleTank/Source/BattleTank/Private/TankTrack.cpp
+++ BattleTank/Source/BattleTank/Private/TankTrack.cpp
@@ -3,7 +3,27 @@
 #include "TankTrack.h"
 #include "BattleTank.h"
 
+UTankTrack::UTankTrack()
+{
+	PrimaryComponentTick.bCanEverTick = true;
+}
 
+void UTankTrack::TickComponent(float DeltaTime, enum ELevelTick TickType, FActorComponentTickFunction *ThisTickFunction)
+{
+	Super::TickComponent(DeltaTime, TickType, ThisTickFunction);
+	auto RightVector = GetRightVector();
+	auto TankRoot = Cast<UStaticMeshComponent>(GetOwner()->GetRootComponent());
+	
+	auto SlippageSpeed = FVector::DotProduct(RightVector, GetComponentVelocity());
+
+	//Opposite force acceleration
+	auto CorrectionAcceleration = -SlippageSpeed / DeltaTime * RightVector;
+
+	auto CorrectionForce = (TankRoot->GetMass() * CorrectionAcceleration) / 2;
+	TankRoot->AddForce(CorrectionForce);
+	
+	
+}
 
 
 void UTankTrack::SetThrottle(float Throttle)
